79ad925c95e741bdfc3d85fcc1307ff8fb472100
Quantum <quantum2048@gmail.com>
2021-01-30 04:24:28 -0500

    [client] generate object file containing keybinding help text

--- client/cmake/MakeObject.cmake
+++ client/cmake/MakeObject.cmake
@@ -1,3 +1,23 @@
+function(make_single_object in_f sym_in out_f out_h)
+  string(REGEX REPLACE "[/.]" "_" ld_sym  ${in_f})
+
+  add_custom_command(OUTPUT ${out_f}
+    COMMAND ${CMAKE_LINKER} -r -b binary -o ${out_f} ${in_f}
+    COMMAND ${CMAKE_OBJCOPY} --rename-section .data=.rodata,CONTENTS,ALLOC,LOAD,READONLY,DATA ${out_f} ${out_f}
+    COMMAND ${CMAKE_OBJCOPY} --redefine-sym _binary_${ld_sym}_start=b_${sym_in} ${out_f} ${out_f}
+    COMMAND ${CMAKE_OBJCOPY} --redefine-sym _binary_${ld_sym}_end=b_${sym_in}_end ${out_f} ${out_f}
+    COMMAND ${CMAKE_OBJCOPY} --strip-symbol _binary_${ld_sym}_size ${out_f} ${out_f}
+    DEPENDS ${in_f}
+    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+    COMMENT "Creating object from ${in_f}"
+    VERBATIM
+  )
+
+  file(WRITE  ${out_h} "extern const char b_${sym_in}[];\n")
+  file(APPEND ${out_h} "extern const char b_${sym_in}_end[];\n")
+  file(APPEND ${out_h} "#define b_${sym_in}_size (b_${sym_in}_end - b_${sym_in})\n")
+endfunction()
+
 function(make_object out_var)
   set(result)
   set(result_h)
@@ -7,21 +27,7 @@ function(make_object out_var)
     set(out_f "${CMAKE_CURRENT_BINARY_DIR}/${out_f}.o")
     string(REGEX REPLACE "[/.]" "_" sym_in  ${in_f})
 
-    add_custom_command(OUTPUT ${out_f}
-      COMMAND ${CMAKE_LINKER} -r -b binary -o ${out_f} ${in_f}
-      COMMAND ${CMAKE_OBJCOPY} --rename-section .data=.rodata,CONTENTS,ALLOC,LOAD,READONLY,DATA ${out_f} ${out_f}
-      COMMAND ${CMAKE_OBJCOPY} --redefine-sym _binary_${sym_in}_start=b_${sym_in} ${out_f} ${out_f}
-      COMMAND ${CMAKE_OBJCOPY} --redefine-sym _binary_${sym_in}_end=b_${sym_in}_end ${out_f} ${out_f}
-      COMMAND ${CMAKE_OBJCOPY} --strip-symbol _binary_${sym_in}_size ${out_f} ${out_f}
-      DEPENDS ${in_f}
-      WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-      COMMENT "Creating object from ${in_f}"
-      VERBATIM
-    )
-
-    file(WRITE  ${out_h} "extern const char b_${sym_in}[];\n")
-    file(APPEND ${out_h} "extern const char b_${sym_in}_end[];\n")
-    file(APPEND ${out_h} "#define b_${sym_in}_size (b_${sym_in}_end - b_${sym_in})\n")
+    make_single_object("${in_f}" "${sym_in}" "${out_f}" "${out_h}")
 
     get_filename_component(h_dir ${out_h} DIRECTORY)
     list(APPEND result_h ${h_dir})
